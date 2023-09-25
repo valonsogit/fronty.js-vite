e2b1188f18eb8c684aa8fdbd49292b4a50720d57
valonso <valonsodev@gmail.com>
2023-09-25 19:39:07 +0200

    Remove plugin inspection

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import compileHandlebars from "./handlebars.plugin";
-import Inspect from "vite-plugin-inspect";
 export default defineConfig({
-    plugins: [Inspect(), compileHandlebars()],
+    plugins: [compileHandlebars()],
 });
